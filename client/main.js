fa1ccdc9c79e063aa84c72b0ca13beea0efe1abe
Samson <smwsamson@outlook.com>
2021-07-18 16:26:11 -0400

    first commit

--- client/main.js
+++ client/main.js
@@ -3,9 +3,9 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Session } from 'meteor/session';
 import 'meteor/jkuester:blaze-bs4';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css' // this is the default BS theme as example
+import 'bootstrap/dist/css/bootstrap.css' 
 import popper from 'popper.js';
-global.Popper = popper // fixes some issues with Popper and Meteor
+global.Popper = popper 
 import './main.html';
 import '../lib/collection.js';
 import'../lib/accounts-ui.js';
